9fe6fc50b3463968fad59387bde8976c824d36a3
Petr Sturc <petr.sturc@gmail.com>
2021-01-02 15:38:17 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,53 @@
-function resetBuffer () {
-    buff = []
-    for (let index = 0; index <= bufflen; index++) {
-        buff[index] = 0
-    }
+bluetooth.onBluetoothConnected(function () {
+    connected = 1
+    starttime = input.runningTime()
+    basic.showString("C")
+})
+bluetooth.onBluetoothDisconnected(function () {
+    connected = 0
+    basic.showString("D")
+})
+input.onButtonPressed(Button.A, function () {
+    bluetooth.startUartService()
+    basic.showString("B")
+})
+function transmitData () {
+    timestamp = input.runningTime() - starttime
+    message.setNumber(NumberFormat.Int8LE, 0, message_type);
+message.setNumber(NumberFormat.Int32BE, 1, timestamp);
+message.setNumber(NumberFormat.Int16BE, 5, value1);
+message.setNumber(NumberFormat.Int16BE, 7, 0);
+message.setNumber(NumberFormat.Int16BE, 9, 0);
+bluetooth.uartWriteBuffer(message)
 }
-let buff: number[] = []
-let bufflen = 0
-bufflen = 30000
-let index = 0
-resetBuffer()
+input.onButtonPressed(Button.B, function () {
+    serial.redirectToUSB()
+    serial.setBaudRate(BaudRate.BaudRate115200)
+    basic.showString("S")
+})
+let value3 = 0
+let value2 = 0
+let starttime = 0
+let connected = 0
+let value4 = 0
+let value1 = 0
+let timestamp = 0
+let message_type = 0
+let message = pins.createBuffer(11);
 basic.forever(function () {
-    buff[index] = pins.analogReadPin(AnalogPin.P0)
-    index += 1
-    index = index % bufflen
+    value1 = pins.analogReadPin(AnalogPin.P0)
+    value2 = pins.analogReadPin(AnalogPin.P1)
+    value3 = pins.analogReadPin(AnalogPin.P2)
+    serial.writeLine("" + timestamp + "," + value1 + "," + value2 + "," + value3)
+    if (connected == 1) {
+        message_type = 1
+        transmitData()
+        message_type = 2
+        value1 = value2
+        transmitData()
+        message_type = 3
+        value1 = value3
+        transmitData()
+    }
     basic.pause(1000)
 })
